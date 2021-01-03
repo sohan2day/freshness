c530e9ec48b6ad56dfa37d6821c37ca74efd1986
sohan2day <74815503+sohan2day@users.noreply.github.com>
2021-01-03 00:45:06 -0800

    Add files via upload
    This is my new or first project in psd to html.I am very New in this platform

---
+++ Practice 1/js/main.js
@@ -0,0 +1,10 @@
+
+// bx slider js here happenning is start
+$(document).ready(function(){
+    $('.slider_are').bxSlider();
+    $('#main_menu').meanmenu({
+      meanMenuContainer:'#mobile_menu',
+      meanScreenWidth:991
+    });
+
+  });
